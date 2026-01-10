9cf17cb0fecb87f1fa284b22ed89612a1c121a57
Claire Vanderwood <clairejvanderwood@gmail.com>
2026-01-09 23:28:18 -0500

    add js file

---
+++ main.js
@@ -0,0 +1,38 @@
+// Store a reference to the <h1> in a variable
+const myHeading = document.querySelector("h1");
+// Update the text content of the <h1>
+myHeading.textContent = "Orca!";
+
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/orca.png") {
+    myImage.setAttribute("src", "images/wild.png");
+  } else {
+    myImage.setAttribute("src", "images/orca.png");
+  }
+});
+
+let myButton = document.querySelector("button");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Welcome to the pod, ${myName}!`;
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Welcome to the pod, ${storedName}!`;
+}
+
+myButton.addEventListener("click", () => {
+  setUserName();
+});
